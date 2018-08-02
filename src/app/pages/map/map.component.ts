9546cc2b425f92e51073bba8142def05ae6de0ab
mshabab89 <mmshabab90@gmail.com>
2018-08-03 00:06:33 +0200

    slight mod and revert done

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
 
   title = 'My first AGM project';
   lat = 63.4332981;
   lng = 10.3951055;
 
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
   }
 
 }
